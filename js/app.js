657149dbdad1727998f71c485a5139840186da4e
Emadeddin-Beshtawi <engemadeddin@gmail.com>
2021-07-27 16:29:17 +0300

    uplab03ex

--- js/app.js
+++ js/app.js
@@ -1,7 +1,7 @@
 "use strict";
 let userName = prompt ('What is your name ?');
 console.log (userName)
-alert('Hello '+userName+' Welcome to my website! :)')
+alert('Hello '+userName +' Welcome to my website! :)')
 
 let q1 = prompt ('Does my Name start with E ?').toLocaleLowerCase();
 console.log (q1)
@@ -72,5 +72,50 @@ switch (q5) {
     alert ('Your answer is wrong :(');
     break;
 }
+
+let q6 = Number(prompt ('Can you guess What my favorite number between 1-15 is ?'));
+
+//while ( q6 !== 12){prompt ('Your answer is not correct please enter a number between 1-15 :')}
+let counter =0 
+for(let i=0; i<3 ;i++){ 
+    if (q6===12)
+    {
+        alert('You guessed the right number!');counter++; console.log (counter);
+        break;
+    }
+
+   else if (q6>12) {
+       q6=Number(prompt ('Your answer is too high :'))
+    }
+
+   else {
+       q6=Number(prompt ('Your answer is too low:'))
+    }
+    
+};
+
+alert ('The correct answer is 12 :) !');
+
+let favMath = ['Linear Algebra', 'Number Theory', 'ODEs', 'PDEs', 'Topology', 'Prob. &Stat.', 'Math. Methods']
+
+let q7 = prompt ('My Favorite Math course is ( linear algebra, Number Theory, ODEs, PDEs, Topology, Prob. &Stat., Math. Methods ) ?').toLocaleLowerCase();
+
+for( let x=0; x<5;x++){
+
+if (q7==='linear algebra'){
+    alert('Your answer is correct :)');counter++; console.log (counter);
+    break;
+}
+else if (q7!=='linear algebra') {
+    prompt ('Your answer is incorrect try again:')
+}
+
+
+
+};
+
+alert ('The correct answer is linear algebra :) !');
+
+alert ('Your final score out of 7 is : '+ counter);
 
 alert('Thank you '+ userName+' ')
