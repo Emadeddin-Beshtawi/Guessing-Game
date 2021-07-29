cbf84910759bd4fb1bd62f7c4bd9b6542cbbb35b
Emadeddin-Beshtawi <engemadeddin@gmail.com>
2021-07-29 11:45:46 +0300

    updated function for all questions

--- js/app.js
+++ js/app.js
@@ -1,180 +1,152 @@
 "use strict";
-let counter =0 
+let userName = prompt('What is your Name ?');
+console.log(userName)
+alert('Hello ' + userName + ' Welcome to my website! :)')
 
-let userName = prompt ('What is your name ?');
-console.log (userName)
-alert('Hello '+userName +' Welcome to my website! :)')
+let counter = 0;
 
-let q1 = prompt ('Does my Name start with E ?').toLocaleLowerCase();
-console.log (q1)
+function nameAns() {
+let q1 = prompt('Does my Name start with E ?').toLocaleLowerCase();
+//console.log (q1)
 switch (q1) {
     case 'yes':
-    alert ('Your answer is correct!');
-    counter++;
-
-    break;
+        alert('Your answer is correct!'); counter++; console.log(counter);//console.log ('Your answer is correct!');
+        break;
     case 'y':
-    alert ('Your answer is correct!');
-    counter++;
-
-    break;
+        alert('Your answer is correct!'); counter++; console.log(counter);//console.log ('Your answer is correct!');
+        break;
     default:
-    alert ('Your answer is wrong :(');
-    break;
+        alert('Your answer is wrong :(');
+        break;
 }
+}
+nameAns();
 
-let q2 = prompt ('Do I love cats ?').toLocaleLowerCase();
-console.log (q2)
+
+function animAns () {
+let q2 = prompt('Do I love cats ?').toLocaleLowerCase();
+//console.log (q2)
 switch (q2) {
     case 'yes':
-    alert ('Your answer is correct!');
-    counter++;
-
-    break;
+        alert('Your answer is correct!'); counter++; console.log(counter);//console.log ('Your answer is correct!');
+        break;
     case 'y':
-    alert ('Your answer is correct!');
-    counter++;
-
-    break;
+        alert('Your answer is correct!'); counter++; console.log(counter);//console.log ('Your answer is correct!');
+        break;
     default:
-    alert ('Your answer is wrong :(');
-    break;
+        alert('Your answer is wrong :(');
+        break;
 }
+}
+animAns();
 
-let q3 = prompt ('Did I graduate from JUST?').toLocaleLowerCase();
-console.log (q3)
+function gradAns(){
+let q3 = prompt('Did I graduate from JUST?').toLocaleLowerCase();
+//console.log (q3)
 switch (q3) {
     case 'no':
-    alert ('Your answer is correct!');
-    counter++;
-
-    break;
+        alert('Your answer is correct!'); counter++; console.log(counter);//console.log ('Your answer is correct!');
+        break;
     case 'n':
-    alert ('Your answer is correct!');
-    counter++;
-
-    break;
+        alert('Your answer is correct!'); counter++; console.log(counter);//console.log ('Your answer is correct!');
+        break;
     default:
-    alert ('Your answer is wrong :(');
-    break;
+        alert('Your answer is wrong :(');
+        break;
 }
+}
+gradAns();
 
-let q4 = prompt ('Is Math my major ?').toLocaleLowerCase();
-console.log (q4)
+function mathAnsw (){
+let q4 = prompt('Did I Study Math?').toLocaleLowerCase();
+//console.log (q4)
 switch (q4) {
     case 'yes':
-    alert ('Your answer is correct!');
-    counter++;
-
-    break;
+        alert('Your answer is correct!'); counter++; console.log(counter); //console.log ('Your answer is correct!');
+        break;
     case 'y':
-    alert ('Your answer is correct!');
-    counter++;
-
-    break;
+        alert('Your answer is correct!'); counter++; console.log(counter); //console.log ('Your answer is correct!');
+        break;
     default:
-    alert ('Your answer is wrong :(');
-    break;
+        alert('Your answer is wrong :(');
+        break;
 }
+}
+mathAnsw();
 
-let q5 = prompt ('Can I Speak German ').toLocaleLowerCase();
-console.log (q5)
+function langAnsw (){
+let q5 = prompt('Can I Speak German ?').toLocaleLowerCase();
+//console.log (q5)
 switch (q5) {
     case 'no':
-    alert ('Your answer is correct!');
-    counter++;
-    break;
+        alert('Your answer is correct!'); counter++; console.log(counter);//console.log ('Your answer is correct!');
+        break;
     case 'n':
-    alert ('Your answer is correct!');
-    counter++;
-    break;
+        alert('Your answer is correct!'); counter++; console.log(counter);//console.log ('Your answer is correct!');
+        break;
     default:
-    alert ('Your answer is wrong :(');
-    break;
+        alert('Your answer is wrong :(');
+        break;
 }
+}
+langAnsw ();
 
-let q6 = Number(prompt ('Can you guess What my favorite number between 1-15 is ?'));
+function numAnsw () {
+let q6 = Number(prompt('Can you guess What my favorite number between 1-15 is ?'));
 
-//while ( q6 !== 12){prompt ('Your answer is not correct please enter a number between 1-15 :')}
-for(let i=0; i<3 ;i++){ 
-    if (q6===12)
-    {
-        alert('You guessed the right number!');counter++; console.log (counter);
+//while ( q6 !== 12){Number(prompt ('Your answer is not correct please enter a number between 1-15 :'))}
+
+for (let i = 0; i < 4; i++) {
+    if (q6 === 12) {
+        alert('You guessed the right number!'); counter++; console.log(counter);
         break;
     }
 
-   else if (q6>12) {
-       q6=Number(prompt ('Your answer is too high :'))
+    else if (q6 > 12) {
+        q6 = Number(prompt('Your answer is too high :'))
     }
 
-   else {
-       q6=Number(prompt ('Your answer is too low:'))
+    else {
+        q6 = Number(prompt('Your answer is too low:'))
     }
-    
-};
-
-alert ('The correct answer is 12 :) !');
-
-let favMath = ['Linear Algebra', 'Number Theory', 'ODEs', 'PDEs', 'Topology', 'Prob. &Stat.', 'Math. Methods']
-
-let q7 = prompt ('My Favorite Math course is ( linear algebra, Number Theory, ODEs, PDEs, Topology, Prob. &Stat., Math. Methods ) ?');
-
-for( let x=0; x<5;x++){
-
-if (q7==='linear algebra'){
-    alert('Your answer is correct :)');counter++; console.log (counter);
-    break;
-}
-else if (q7!=='linear algebra') {
-    alert ('Your answer is incorrect try again:')
-
-    q7 = prompt ('My Favorite Math course is ( linear algebra, Number Theory, ODEs, PDEs, Topology, Prob. &Stat., Math. Methods ) ?');
-
-}
-
-
 
 };
 
-alert ('The correct answer is linear algebra :) !');
-
-function shape() {
-    let q8= prompt('Triangle is My favshape ?').toLowerCase();
-
-    while (q8 != 'yes' && q8 != 'y' && q8 != 'no' && q8 != 'n' )
-    {
-        q8= prompt('Input yes or no');
-    }
-
-    switch (q8) {
-        case 'yes':
-        alert ('Your answer is correct!');
-        counter++;
-
-        break;
-        case 'y':
-        alert ('Your answer is correct!');
-        counter++;
-
-        break;
-
-        case 'no':
-        alert ('Your answer is incorrect!');
-        break;
-        case 'n':
-        alert ('Your answer is incorrect!');
-        break;
-        
-    }
-
-    
+alert('The correct answer is 12 ');
 }
+numAnsw();
 
 
-shape();
+function favAnsw (){
+let favshape = ['c', 'o', 's', 'w', 'z']
+
+let q7 = prompt('My Fav letter is? (c, o, s, w, z)').toLocaleLowerCase();
+let test = false
 
 
-alert ('Your final score out of 8 is : '+ counter);
+
+for (let x = 0; x < 6; x++) {
+
+    for (let z = 0; z < favshape.length; z++) {
+        console.log(favshape[z]);
+        if (q7 === 'w') {
+            alert('Your Answer is Correct :)');
+            counter++;
+            console.log(counter);
+            test = true;
+            break;
+        }
+    }
+    if (test == true) {
+        break;
+    }
+    q7=prompt('Incorrect Answer')
+}
+}
+favAnsw();
+
+
+alert ('Your final score out of 7 is : '+ counter);
 
 alert('Thank you '+ userName+' ')
 
